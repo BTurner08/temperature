3f8d7b215a9dd623f39da82e1d0a55ce6351c4e6
BTurner08 <mrbturner2013@yahoo.com>
2024-04-07 01:06:04 -0400

    Homework 1 calculator

--- script.js
+++ script.js
@@ -6,15 +6,24 @@ function calculate(){//begining of function
 
 var number1= Number (prompt("Enter the first number:"));// Number() convert into number
 var number2= Number (prompt("Enter the second number:"));
-var option= prompt("Enter the option: 1) Sum, 2) Sub");
+var option= prompt("Enter the option: 1) Addition, 2) Subtraction, 3)Multiply, 4)Divide");
 
 //console.log("userName");
 //console.log(userName);
 if(option == 1){
+console.log("Addition results")
 console.log(number1+number2);//math operation
 }
 if(option == 2){
     console.log("Subtraction results")
     console.log(number1-number2);
+}
+if(option == 3){
+    console.log("Multiplication results")
+    console.log(number1*number2);
+}
+if(option == 4){
+    console.log("Division results")
+    console.log(number1/number2);
 }
 }//end of function
